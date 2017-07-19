7573f24a2c980403ad9084e579da009e57c562e6
ToanDoan <toandoan.coder@gmail.c>
2017-07-19 15:08:42 +0700

    config ci

---
+++ scripts/chatwork.rb
@@ -0,0 +1,5 @@
+	require "chatwork"
+
+	# Create message
+	ChatWork.api_key = "8f247d4a711fb2326012a8ad47243db3"
+	ChatWork::Message.create(room_id: 79465686, body: "[info][To:2309461] [To:2357769] [To:2110258]\nCI Build Success :x!  \n #{ARGV[0]} [/info]")
